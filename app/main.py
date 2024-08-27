3806ae57fc3624e136edd77d2739d85ef3da6be4
LoxPipOCHKA <ilya.kuzneczoff@gmail.com>
2024-08-27 14:27:18 +0300

    Data validation

--- app/main.py
+++ app/main.py
@@ -1,19 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Optional
+from pydantic import  BaseModel
 
 app = FastAPI()
 
 @app.get('/variety')
-async def variety():
+async def variety(variety: int):
     return 'ПРИВЕТ ПИДОРЫ ручка variety'
 
 @app.get('/beers')
-async def beers():
+async def beers(beers: str,
+                variety: Optional[int]= Query(None, ge=1, le=8)):
     return 'ручка beers GHBDTN PIDOR'
 
 @app.get('/beers/{beer_id}')
-async def beer_id():
+async def beer_id(beer_id: int):
     return 'ручка beer_id пидор'
 
+
+class Sreview(BaseModel):
+    name_beer: str
+    comment: str
+    nick: str
+    estimation: int
+
+
 @app.post('/beers/{beer_id}/review')
-async def review():
+async def review(beers: str,
+                 beer_id: int,
+                review: Sreview):
     return 'endpoint review pidoras'
+
