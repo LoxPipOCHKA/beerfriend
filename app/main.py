085167fddf66d5210d1ffe33525be1f082225fe0
LoxPipOCHKA <ilya.kuzneczoff@gmail.com>
2024-08-29 22:18:49 +0300

    input date return (#4)
    * input date return
    
    * worker endpoints

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from beers.Review import Review
 
 app = FastAPI()
 
 @app.get('/variety')
 async def variety():
-    return 'ПРИВЕТ ПИДОРЫ ручка variety'
+    return 'endpoint variety='
 
 @app.get('/beers')
-async def beers():
-    return 'ручка beers GHBDTN PIDOR'
+async def beers(
+                variety_id: int = Query(ge=1, le=8)):
+    return 'endpoint beers, variety_id = ' + str(variety_id)
 
 @app.get('/beers/{beer_id}')
-async def beer_id():
-    return 'ручка beer_id пидор'
+async def beer_id(beer_id: int):
+    return 'endpoint beer_id= '+ str(beer_id)
 
 @app.post('/beers/{beer_id}/review')
-async def review():
-    return 'endpoint review pidoras'
+async def review(
+                beer_id: int,
+                review: Review):
+    return 'endpoint review : ' + 'nick = '+review.nick + ', comment = ' + review.comment + ', estimation = ' + str(review.estimation)
